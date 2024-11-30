1c8c7c7f330642cd8018cc2d1d2fdc360f7b4b3b
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-11-30 19:39:08 +0500

    Update chatbot.tsx

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -139,15 +139,47 @@ const Chatbot: React.FC = () => {
         >
           <h3 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">Documentation</h3>
           <p className="text-gray-700 dark:text-gray-300">
-            This chatbot is powered by an advanced AI model tailored for Data Science-related queries.
-            You can ask it any question regarding Data Science and it will respond with detailed answers.
+              This chatbot is powered by an advanced AI model tailored for Data Science-related queries.
+              You can ask it any question regarding Data Science and it will respond with detailed answers.
           </p>
+          <h4 className="text-xl font-semibold text-gray-900 dark:text-white mt-4">How to Use</h4>
+          <ul className="list-disc list-inside text-gray-700 dark:text-gray-300 mt-2">
+            <li>
+              API Endpoint: <code>jahanzebahmed22.pythonanywhere.com/response</code>
+            </li>
+            <li>
+              Expected Request Type: <code>POST</code>
+            </li>
+            <li>
+              API Key in Header: <code>'x-api-key': '879479379749734597'</code>
+            </li>
+            <li>
+              <strong>Expected Request Body:</strong>
+              <pre className="bg-gray-100 dark:bg-gray-700 p-3 rounded mt-2 text-sm">
+                    {`{
+                "prompt": "who are you?",
+                "system_prompt": "You are Intelligent",
+                "tokens": 500
+                }`}
+              </pre>
+            </li>
+            <li>
+              <strong>Example Headers:</strong>
+              <pre className="bg-gray-100 dark:bg-gray-700 p-3 rounded mt-2 text-sm">
+                {`{
+                "Content-Type": "application/json",
+                "x-api-key": "879479379749734597"
+                }`}
+              </pre>
+            </li>
+          </ul>
+
 
           <div className="mt-4">
             <motion.button
               onClick={generateApiKey}
               className="bg-emerald-600 hover:bg-emerald-500 active:bg-emerald-700 text-white px-4 py-2 rounded-full"
-              whileHover={{ scale: 1.1 }}
+              whileHover={{ scale: 1.0 }}
               whileTap={{ scale: 0.9 }}
             >
               Generate API Key
@@ -236,6 +268,9 @@ const Chatbot: React.FC = () => {
             <Send />
           </button>
         </div>
+        <p className="text-sm text-center text-gray-600 dark:text-gray-300 mt-6">
+          Bot can make mistakes. Check important info.
+        </p>
       </div>
     </motion.div>
   );
