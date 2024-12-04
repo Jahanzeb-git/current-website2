9ed707ccd19fd132c574eb4cff7ba28183de248b
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-04 23:08:45 +0500

    Update App.tsx
    Added Background images

--- src/App.tsx
+++ src/App.tsx
@@ -35,8 +35,8 @@ function AppContent({ isDocumentationOpen, toggleDocumentation }) {
   const { theme } = useTheme();
 
   // Define background images for light/dark mode
-  const lightImage = "url('your-light-image-url')";
-  const darkImage = "url('your-dark-image-url')";
+  const lightImage = "url('https://images.unsplash.com/photo-1702285566373-bb7744adcbe7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+  const darkImage = "url('https://images.unsplash.com/photo-1647346425804-34383b95644b?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
 
   return (
     <div className="relative min-h-screen flex flex-col">
