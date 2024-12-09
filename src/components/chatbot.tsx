9b34d90e4f91742c494dd1c8ea73de2f2d2668a7
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-09 22:52:11 +0500

    Update chatbot.tsx
    Send Button UI modification

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -160,7 +160,6 @@ const Chatbot: React.FC = () => {
               onClick={() => handleSend(input)}
           >
               <ArrowUpRight className="w-5 h-5 text-gray-800 dark:text-white" />
-            <Send />
           </button>
         </div>
         <div className="flex flex-wrap justify-center gap-2 mb-4">
