6c7a1693893efd75b49b6d3712782e40efb3f8d3
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-11 22:39:46 +0500

    Update chatbot.tsx

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -176,6 +176,12 @@ const Chatbot: React.FC<{ onIntersect: (isVisible: boolean) => void }> = ({ onIn
             >
               Documentation
             </button>
+	    <button
+                className="text-gray-800 dark:text-white font-semibold block mb-2"
+                onClick={closeMenu}
+              >
+                Try Image Generation
+              </button>
             <div>
               <button
                 className="text-gray-800 dark:text-white font-semibold block mb-2"
