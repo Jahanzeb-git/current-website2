020aa0da75fa9190dc0e3a14d5056f74c8447114
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-11 23:06:02 +0500

    Update chatbot.tsx

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -178,7 +178,7 @@ const Chatbot: React.FC<{ onIntersect: (isVisible: boolean) => void }> = ({ onIn
             </button>
 	    <button
               className="text-gray-800 dark:text-white text-lg font-semibold block mb-4"
-              onClick={() => navigate('/ImageGeneration')}
+              onClick={() => navigate('/imagegeneration')}
             >
               Try Image Generation
             </button>
