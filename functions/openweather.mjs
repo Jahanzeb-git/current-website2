f8855cf0b04d4e5f61df8444a37ee80d2eefd4d0
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-19 23:41:23 +0500

    Update openweather.mjs

--- functions/openweather.mjs
+++ functions/openweather.mjs
@@ -1,16 +1,27 @@
 const API_KEY = '3ecba3da1d7e63f012b5734ad68e7c14';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-export const fetchWeather = async (city) => {
+export async function handler(event) {
+  const { city } = JSON.parse(event.body);
   try {
     const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`);
     if (!response.ok) {
-      throw new Error("Failed to fetch weather data");
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: "Failed to fetch weather data" }),
+      };
     }
     const data = await response.json();
-    return data;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
   } catch (error) {
     console.error("Error fetching weather data:", error);
-    return null;
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
   }
-};
+}
+
