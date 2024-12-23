61eb9c04a6383d8d9b8238438669cec9f0aef00e
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-23 21:03:53 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ function AppContent({ isDocumentationOpen, toggleDocumentation }) {
               <Route path="/documentation" element={<Documentation />} /> {/* Add Documentation route */}
               <Route path="/projects/:ai-powered-data-science" element={<ProjectDetail />} />
               <Route path="/projects/:neural-network-visualizer" element={<ProjectDetail2 />} />
-              <Route path="/projects/:id" element={<projectnavigate />} />
+              <Route path="/projects/:id" element={<projectnavigation />} />
               <Route path="/not-found" element={<div>404 - Page Not Found</div>} />
             </Routes>
           </AnimatePresence>
