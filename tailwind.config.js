65970d6ec111ca33ab0e18a7df200a64c2357049
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-24 17:27:24 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,3 +4,14 @@ export default {
   darkMode: 'class',
   plugins: [],
 };
+
+/* Add these to your existing Tailwind extend configuration */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        acorn: ['Acorn', 'sans-serif'],
+      },
+    },
+  },
+}
