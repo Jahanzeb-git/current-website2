c2cd87d1079acf2ce8ccad21d109d4e4c39d9804
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-25 00:50:04 +0500

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -132,16 +132,16 @@ const About = () => {
           <h1 className="text-6xl md:text-9xl font-extrabold mb-8 bg-gradient-to-r from-[#025A4E] to-teal-600 dark:from-emerald-400 dark:to-teal-400 text-transparent bg-clip-text">
             Hello.
           </h1>
-         <h2 className="text-6xl md:text-9xl font-extrabold mb-8 bg-gradient-to-r from-[#025A4E] to-teal-600 dark:from-emerald-400 dark:to-teal-400 text-transparent bg-clip-text">
-          <MapPin className="w-5 h-5 mr-2" />
-          {weather ? (
-            <span>
-              Working remotely from {Math.round(weather.main.feels_like)}° Karachi, Pakistan
-            </span>
-          ) : (
+          <h2 className="text-6xl md:text-9xl font-extrabold mb-8 bg-gradient-to-r from-[#025A4E] to-teal-600 dark:from-emerald-400 dark:to-teal-400 text-transparent bg-clip-text">
+            <MapPin className="w-5 h-5 mr-2" />
+            {weather ? (
+              <span>
+                Working remotely from {Math.round(weather.main.feels_like)}° Karachi, Pakistan
+              </span>
+            ) : (
               <span>Loading...</span>
-          )}
-        </h2>
+            )}
+          </h2>
           <div className="mb-6">
             <motion.div
               className={`text-gray-600 dark:text-gray-300 ${
