250b8624a3edec0721258988838fb2bef66c220a
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-25 00:44:43 +0500

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -132,7 +132,7 @@ const About = () => {
           <h1 className="text-6xl md:text-9xl font-extrabold mb-8 bg-gradient-to-r from-[#025A4E] to-teal-600 dark:from-emerald-400 dark:to-teal-400 text-transparent bg-clip-text">
             Hello.
           </h1>
-         <h2 className="flex items-center mb-4 text-gray-600 dark:text-gray-300">
+         <h2 className="text-6xl md:text-9xl font-extrabold mb-8 bg-gradient-to-r from-[#025A4E] to-teal-600 dark:from-emerald-400 dark:to-teal-400 text-transparent bg-clip-text">
           <MapPin className="w-5 h-5 mr-2" />
           {weather ? (
             <span>
