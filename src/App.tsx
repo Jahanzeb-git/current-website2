f8c272e32c6587d49290f5608e402e3dbeca5fcf
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-12-24 21:04:06 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function AppContent({ isDocumentationOpen, toggleDocumentation }) {
   const { theme } = useTheme();
 
   // Define background images for light/dark mode
-  const lightImage = "url('https://img.freepik.com/free-vector/vibrant-summer-ombre-background-vector_53876-105765.jpg?t=st=1735055263~exp=1735058863~hmac=d6288d685ae6e6bbef8dc756649bb8a9b2f316e4a1101eda72c3997f502b3193&w=740')";
+  const lightImage = "url('https://i.imgur.com/04tpiXa.jpeg')";
   const darkImage = "url('https://images.unsplash.com/photo-1647346425804-34383b95644b?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
 
   return (
